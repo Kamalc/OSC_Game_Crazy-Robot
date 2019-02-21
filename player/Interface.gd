a3cf420ce5256663d6307660811dcc75a51a6d9f
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2019-02-22 00:07:53 +0200

    fixed health bar

--- player/Interface.gd
+++ player/Interface.gd
@@ -1,7 +1,11 @@
 extends Spatial
 
-
+func _ready():
+	if is_network_master():
+		$Bars.show()
+		#$Counters.show()
 
 func _on_robot_took_damage(health):
 	$Bars/LifeBar/TextureProgress.value=health
 	$Bars/LifeBar/Count/Number.text=str(health)+"/100"
+
